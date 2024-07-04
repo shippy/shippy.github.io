03aa98b01709e13f0f4454e221b00c803ff9f7ec
Ubong Sylvester <ubongsly@gmail.com>
2024-07-04 12:48:15 +0100

    completed side_projects page

--- src/lib/utils/formatCollection.ts
+++ src/lib/utils/formatCollection.ts
@@ -2,18 +2,23 @@ import type { CollectionEntry } from "astro:content";
 
 interface OptionProps {
   sortByDate?: boolean;
+  sortParams?: string;
   limit?: number;
 }
 
 export function formatCollection<Type>(
   collection: Type[],
-  { sortByDate = true, limit = undefined }: OptionProps = {}
+  {
+    sortByDate = true,
+    sortParams = "date",
+    limit = undefined,
+  }: OptionProps = {},
 ) {
   // sortByDate or randomize
   if (sortByDate) {
     collection.sort(
       //@ts-ignore
-      (a, b) => new Date(b.data.date) - new Date(a.data.date)
+      (a, b) => new Date(b.data[sortParams]) - new Date(a.data[sortParams]),
     );
   } else {
     collection.sort(() => Math.random() - 0.5);
